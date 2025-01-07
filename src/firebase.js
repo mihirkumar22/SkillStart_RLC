1aa80285b17284d64aae8900cebf0fea4f25343b
redbean3548 <mihirkumarmk1@gmail.com>
2025-01-07 14:01:12 -0500

    final

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAp8f8aPbLtyXPefEQbcfBliLmm93HwnMw",
-  authDomain: "website-616c9.firebaseapp.com",
-  projectId: "website-616c9",
-  storageBucket: "website-616c9.firebasestorage.app",
-  messagingSenderId: "248362649618",
-  appId: "1:248362649618:web:6ebff7b8f2602cc8f1b0cb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
